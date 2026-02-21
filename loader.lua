c6a5711f8424e458bf6e10127f74c600c726853d
B0NBunny <dustinbk@icloud.com>
2026-02-21 02:19:43 -0500

    helpful debug

--- loader.lua
+++ loader.lua
@@ -79,7 +79,11 @@ _G.IrisModules = IrisModules
 
 for moduleName, url in pairs(IrisURLs) do
     if not table.find(MustBeLoadedManually, moduleName) then
-        IrisModules[moduleName] = loadstring(Get(url))()
+        local got = loadstring(Get(url))
+        if not got then
+            error("[Error] Failed to load module: " .. moduleName .. " from URL: " .. url)
+        end
+        IrisModules[moduleName] = got()
     end
 end
 
