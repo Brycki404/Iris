e96b87ae63cf745cff36367a4ef726e80e0bb5e5
B0NBunny <dustinbk@icloud.com>
2026-02-21 00:27:45 -0500

    Loader complete. I don't need to touch that script anymore I don't think.

--- loader.lua
+++ loader.lua
@@ -1,11 +1,9 @@
-local OldHttpGet = game.HttpGet
-print("OldHttpGet:", OldHttpGet)
-local OldHttpGetAsync = game.HttpGetAsync
-print("OldHttpGetAsync:", OldHttpGetAsync)
-local OldGet = OldHttpGet or OldHttpGetAsync or nil
-assert(OldGet, "No HttpGet function found on game object.")
--- Load BetterLib first (if it's not already loaded), since every other loaded stuff will depend on it. If BetterLib fails to load, everything else won't work, but at least the error will be more informative.
-loadstring(OldGet(game, "https://raw.githubusercontent.com/Brycki404/BetterLib/refs/heads/main/main.lua", true))()
+if not _G.BetterLib or not _G.Get then
+    local OldGet = game.HttpGet or game.HttpGetAsync or nil
+    assert(OldGet, "No HttpGet function found.")
+    -- Load BetterLib first (if it's not already loaded), since every other loaded stuff will depend on it. If BetterLib fails to load, everything else won't work, but at least the error will be more informative.
+    loadstring(OldGet(game, "https://raw.githubusercontent.com/Brycki404/BetterLib/refs/heads/main/main.lua", true))()
+end
 -- Begin Script:
 
 local IrisInitUrl = "https://raw.githubusercontent.com/Brycki404/Iris/refs/heads/main/lib/init.lua"
